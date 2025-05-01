2c9933478e9bdf91b3e06411c70aed507cc1dce5
JSC0322 <158159971+JSC0322@users.noreply.github.com>
2025-05-01 22:57:27 +0800

    Update products_data.py

--- products_data.py
+++ products_data.py
@@ -4,20 +4,23 @@ products = {
         "name": "珍珠奶茶冰淇淋蛋糕",
         "image": "https://hackmd.io/_uploads/rylaxj1egg.jpg",
         "short": "Q彈珍珠＋香濃奶茶，冰火交融的口感！",
-        "detail": "使用北海道鮮奶油與熬煮6小時的黑糖珍珠製作，6吋/8吋皆可選擇，風味濃郁不膩。"
+        "detail": "使用北海道鮮奶油與熬煮6小時的黑糖珍珠製作，6吋/8吋皆可選擇，風味濃郁不膩。",
+        "category": "ice_cream_cake"
     },
     "matcha": {
         "id": "matcha",
         "name": "抹茶冰淇淋蛋糕",
         "image": "https://hackmd.io/_uploads/SyHmFikeeg.png",
         "short": "小山園抹茶＋紅豆內餡，苦甜層次豐富。",
-        "detail": "採用日本宇治抹茶，搭配自家熬煮紅豆餡，成熟大人氣的推薦選擇。"
+        "detail": "採用日本宇治抹茶，搭配自家熬煮紅豆餡，成熟大人氣的推薦選擇。",
+        "category": "ice_cream_cake"
     },
     "berries": {
         "id": "berries",
         "name": "綜合莓果冰淇淋蛋糕",
         "image": "https://hackmd.io/_uploads/rJdDPjkgge.jpg",
         "short": "0000000000000000000",
-        "detail": "0000000000000000000000000000"
+        "detail": "0000000000000000000000000000",
+        "category": "ice_cream_cake"
     }
 }
