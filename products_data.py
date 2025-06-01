e7ed660e4837029124a6a0205a3f529b5d54272e
JSC0322 <158159971+JSC0322@users.noreply.github.com>
2025-06-01 19:07:31 +0800

    Update products_data.py

--- products_data.py
+++ products_data.py
@@ -1,15 +1,23 @@
 products = {
-    "pearl": {
-        "id": "pearl",
+    "vanilla-chocolate": {
+        "id": "vanillachocolate",
+        "name": "香草巧克力冰淇淋蛋糕",
+        "image": "https://lh6.googleusercontent.com/PpNP3MtQr3bDhgEBBVWBt6x9gH-6X7x49EQ87n_ZcuVkQm-pjKzkAjVTy6wyh_k20cgXj28-rLkORnvNSMdZN7y2PB_vIYMEk1NLGBU7R0o11MmtOeuZiwbj_9F6PvtnpkHd7nex99HsASgTNK-9J8kj99cmQJiSZR_nv7LBQyxCjMMJhQ=w260?key=f3LMdrebQNMgqn96gxJSfw",
+        "short": "香草的柔滑遇上濃郁巧克力，層層交融，甜而不膩，入口即化的經典冰淇淋蛋糕。",
+        "detail": "香草牛奶冰淇淋的細緻奶香，搭配法芙娜巧克力的濃郁可可香，層層堆疊出香濃又滑順的口感。蛋糕底層加入巧酥脆片，增添咬感，是一款大人小孩都會愛上的經典冰淇淋蛋糕。",
+        "category": "ice_cream_cake"
+    }
+    "pearl-milk-tea": {
+        "id": "pearlmilktea",
         "name": "珍珠奶茶冰淇淋蛋糕",
         "image": "https://hackmd.io/_uploads/rylaxj1egg.jpg",
         "short": "Q彈珍珠＋香濃奶茶，冰火交融的口感！",
-        "detail": "現熬茶香融合奶香滑順，搭配Q彈珍珠，還原最熟悉的珍珠奶茶風味冰淇淋蛋糕。",
+        "detail": "現熬茶香融合奶香滑順，搭配Q彈珍珠，還原最熟悉的珍珠奶茶冰淇淋蛋糕。",
         "category": "ice_cream_cake"
     },
     "matcha": {
         "id": "matcha",
-        "name": "抹茶冰淇淋蛋糕",
+        "name": "小山園抹茶拿鐵冰淇淋蛋糕",
         "image": "https://hackmd.io/_uploads/SyHmFikeeg.png",
         "short": "小山園抹茶＋紅豆內餡，苦甜層次豐富。",
         "detail": "採用日本宇治抹茶，搭配自家熬煮紅豆餡，成熟大人氣的推薦選擇。",
@@ -23,20 +31,28 @@ products = {
         "detail": "綜合莓果酸甜濃郁，搭配香濃冰淇淋與繽紛水果，層層口感，清爽又幸福。",
         "category": "ice_cream_cake"
     },
-    "strawberry": {
-        "id": "strawberry",
+    "milk-strawberry": {
+        "id": "milkstrawberry",
         "name": "草莓牛奶冰淇淋蛋糕",
         "image": "https://hackmd.io/_uploads/HJnt1r7eex.jpg",
         "short": "冰涼微甜的天然草莓🍓，幸福美味首選。",
         "detail": "草莓牛奶香甜融合，搭配繽紛水果與冰淇淋球，綿密滑順，入口滿是幸福滋味。",
         "category": "ice_cream_cake"
     },
-    "vanillachocolate": {
-        "id": "vanillachocolate",
-        "name": "香草巧克力冰淇淋蛋糕",
-        "image": "https://lh6.googleusercontent.com/PpNP3MtQr3bDhgEBBVWBt6x9gH-6X7x49EQ87n_ZcuVkQm-pjKzkAjVTy6wyh_k20cgXj28-rLkORnvNSMdZN7y2PB_vIYMEk1NLGBU7R0o11MmtOeuZiwbj_9F6PvtnpkHd7nex99HsASgTNK-9J8kj99cmQJiSZR_nv7LBQyxCjMMJhQ=w260?key=f3LMdrebQNMgqn96gxJSfw",
-        "short": "香草的柔滑遇上濃郁巧克力，層層交融，甜而不膩，入口即化的經典冰淇淋蛋糕。",
-        "detail": "香草牛奶冰淇淋的細緻奶香，搭配法芙娜巧克力的濃郁可可香，層層堆疊出香濃又滑順的口感。蛋糕底層加入巧酥脆片，增添咬感，是一款大人小孩都會愛上的經典冰淇淋蛋糕。",
+    "milk-peanut": {
+        "id": "milkpeanut",
+        "name": "花生牛奶冰淇淋蛋糕",
+        "image": "https://lh5.googleusercontent.com/fEypdajlNOr_VHB-PA5nHUxbk--7tUSJT4wagCbmWQaoKGAen6BCjTthSh77Uo9ciAQKJ-dQV2EpdqQFIqNGdYGDf7tQotFC8JrXx9q5vqUcTSQ15hsCtOxLkvMOIv7CKKKwiPWUnEs8lRWcHGEMwDXQXvP3HnRoPdiizrNmZdyqXqLkgA=w260?key=f3LMdrebQNMgqn96gxJSfw",
+        "short": "濃郁花生醬融合奶香，綿密香滑、香氣十足。",
+        "detail": "嚴選香醇花生醬與鮮奶調和製作，口感濃郁滑順，入口帶有花生顆粒的香氣與咬感。搭配酥脆餅乾底，是一款經典又耐吃的冰淇淋蛋糕，深受大人小孩喜愛。",
+        "category": "ice_cream_cake"
+    },
+    "chocolate-strawberry": {
+        "id": "milkstrawberry",
+        "name": "草莓巧克力冰淇淋蛋糕",
+        "image": "https://lh4.googleusercontent.com/jpxOLDKsRmTNT9x5cK6MnfQk9Px4zlU588_MtjQu7BmE33oBVrlWOyFBJMOcDD0_biwWSwhPLnqH1WY10tyfmJ8AfqgPMF6yx7T3caXa7WpKkEuZ2asY4GG-Vs91lTNqVy8oFftNfrZJ7qoXcMjNdWlZYqjjyooMi_PN14qykb6RGlMLCA=w260?key=f3LMdrebQNMgqn96gxJSfw",
+        "short": "香甜草莓遇上濃醇巧克力，酸甜層次口感超迷人。",
+        "detail": "使用新鮮草莓製成冰淇淋，酸甜清香，搭配法芙娜巧克力冰淇淋的濃郁苦甜，兩種風味交織出完美層次。底層酥脆餅乾增添口感，是一款視覺與味覺兼具的夢幻冰淇淋蛋糕。",
         "category": "ice_cream_cake"
     }
 }
